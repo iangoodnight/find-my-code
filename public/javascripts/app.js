a0d940f7b57468133dd347324550d32cc542ccd1
Ian Goodnight <goodnight.ianb@gmail.com>
2020-07-17 14:27:14 -0400

    added ontouchstart handler

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,17 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-// 	var input = document.querySelector('input');
-// 	input.focus();
-// 	input.addEventListener('keydown', function(e) {
-// 		if (input.value >= 12) {
-// 			if (e.keyCode === 9 || e.keyCode === 13) {
-// 				location.replace(`/${input.value}`);
-// 				input.focus();111280058923	
-// 			};
-// 		};
-// 	});
-
-// });
-
 function app() {
 	var input = document.querySelector('input#search');
 	input.focus();
@@ -24,5 +10,13 @@ function app() {
 		};
 	});
 };
+
+document.body.addEventListener('click', function(e) {
+	document.querySelector('input#search').focus();
+});
+
+document.body.addEventListener('touchstart', function(e) {
+	document.querySelector('input#search').focus();
+});
 
 app();
